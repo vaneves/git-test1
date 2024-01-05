d0d8bb36485d199dbf4fd5b497097ed46c80fd3c
Van Neves <vaneves@vaneves.com>
2024-01-04 21:19:45 -0300

    date

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/hello', (req, res) => {
   res.send('world')
 })
 
+app.get('/date', (req, res) => {
+    res.send(new Date())
+})
+
 app.listen(8899, () => {
   console.log('Server running')
 })
