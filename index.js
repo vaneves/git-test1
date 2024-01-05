a8f2e6443089b94a989644e7803f67454e10adb0
Van Neves <vaneves@vaneves.com>
2024-01-04 23:03:27 -0300

    feat/now start

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/hello', (req, res) => {
   res.send('world')
 })
 
+app.get('/now', (req, res) => {
+  res.send('now')
+})
+
 app.listen(8899, () => {
   console.log('Server running in port 8899')
 })
